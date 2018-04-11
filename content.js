a993c7b2b86fd4d862f9c4af52b7df16a1887a7f
hythamshiehab2 <38292225+hythamshiehab2@users.noreply.github.com>
2018-04-11 17:54:55 +0200

    Add files via upload

---
+++ content.js
@@ -0,0 +1,61 @@
+function generateIdea() {
+    var ideas = ["idea1",
+        "idea2",
+        "idea3",
+        "idea4",
+        "idea5",
+        "idea6",
+        "idea7"
+      ]
+    var randomArrayPosition = Math.floor(Math.random() * ideas.length);
+    var idea = ideas[randomArrayPosition];
+  return idea;
+}
+
+chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
+    console.log("xxxx happening from the extension");
+    var data = request.data || {};
+    console.log('data sent:' + data);
+    console.log('wainting load to complete');
+    console.log(request);
+    console.log(sendResponse);
+    console.log('xxxx');
+    /*
+    likeButton _4jy0 _4jy4 _517h _51sy _42ft
+    */
+    /* for later
+    $(document).ready(function() {
+      $('iframe').load(function() {
+       // do something
+     });
+    });
+    */
+
+    $(document).ready(function() {
+      var ll = document.getElementsByClassName('likeButton')[0];
+      console.log(' found the likeButton class');
+      if(ll)
+      {
+        console.log('will like!');
+        ll.click();
+      }
+    });
+    /*
+    $('textarea').trigger({type: 'keypress', which: 13, keyCode: 13});
+    var t = document.querySelectorAll('textarea')[0];
+    t.innerText = generateIdea();
+    document.getElementById('submit').click();
+    console.log('here');
+
+    sendResponse({data: 'yyyyy', success: true});
+    document.addEventListener('DOMContentLoaded', function() {
+      var t = document.querySelectorAll('textarea')[0];
+      t.innerText ='sdfsdf';
+      console.log('here');
+      //[].forEach.call(linksList, function(header) {
+      //    header.innerHTML = request.data;
+      //});
+      sendResponse({data: 'yyyyy', success: true});
+    });
+    */
+});
---
+++ surf.js
@@ -0,0 +1,51 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('status').textContent = "Extension loaded";
+    var button = document.getElementById('startSurf');
+    button.addEventListener('click', function () {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+          chrome.tabs.update(
+            //tabs[0].id, { url: 'http://localhost/?p=1'}, function(tab) {
+            tabs[0].id, { url: 'https://www.facebook.com/en7erafatamnaldawla'}, function(tab) {
+                chrome.tabs.onUpdated.addListener(function listener(tabId, changeInfo) {
+                if (tabId === tab.id && changeInfo.status == 'complete') {
+                  chrome.tabs.onUpdated.removeListener(listener);
+                  // Now the tab is ready!
+                  chrome.tabs.sendMessage(tabs[0].id, {data: "start"});
+                }
+              });
+            });
+});
+});
+});
+
+            /*
+            chrome.tabs.update(tabs[0].id, {url: 'http://localhost/?p=1'}, function(response){
+                  //chrome.tabs.sendMessage(response);
+                  ttt = response;
+                  document.DOMContentRendered(alert('boo'));
+                  var t = document.querySelectorAll('textarea')[0];
+                  t.innerText ='sdfsdf';
+
+                  chrome.tabs.sendMessage(tabs[0].id, {data: "start"}, function(response) {
+                      //chrome.tabs.sendMessage
+                      $('#status').html('changed data in page');
+                      console.log(response);
+                  });
+                  console.log('res:' + response);
+              });            //chrome.tabs.executeScript(tab.id, {code: "alert('boo');")});
+            //document.addEventListener('DOMContentLoaded', function() {
+              //chrome.tabs.executeScript(tabs[0].id, {code: "document.addEventListener('DOMContentLoaded', function() {alert('test');});"}, function(response) {});
+          //});
+            });
+          });
+        });
+//});
+
+chrome.tabs.query({'active': true}, function(tabs) {
+  chrome.tabs.update(tabs[0].id, {url: 'http://localhost/'});
+  chrome.tabs.getSelected(null, function(tab){
+      chrome.tabs.executeScript(tab.id, {code: "alert('test');"}, function(response) {
+      });
+  });
+});
+*/
