974fbc835c1c55cd53b7f5f66372b77a8632fb97
hythamshiehab2 <hytham.shiehab.2@gmail.com>
2018-04-14 22:10:22 +0200

    starting to use localStorage & BadgeText

--- scripts/surf.js
+++ scripts/surf.js
@@ -12,21 +12,29 @@ document.addEventListener('DOMContentLoaded', function() {
     //var button = document.getElementById('startSurf');
     //var toggleButton = document.getElementById('toggleButton');
     $('#toggleButton').change(function() {
-      //$('#console-event').html('Toggle: ' + $(this).prop('checked'));
-      console.log('xxxxxxx');
-    })
-    button.addEventListener('click', function () {
+      $('#console-event').html('Toggle: ' + $(this).prop('checked'));
+      status = $(this).prop('checked');
+      if(status == 'true')
+      {
+        $('#console-event').html('will r&r!');
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
           chrome.extension.sendMessage({ msg: "startFunc", data: tabs[0] });
-          chrome.tabs.update(
-            //tabs[0].id, { url: 'http://localhost/?p=1'}, function(tab) {
-            tabs[0].id, { url: 'https://www.facebook.com/en7erafatamnaldawla'}, function(tab) {
-                //chrome.tabs.onUpdated.addListener(function listener(tabId, changeInfo) {
-                //if (tabId === tab.id && changeInfo.status == 'complete') {
-                  //chrome.tabs.onUpdated.removeListener(listener);
-                  // Now the tab is ready!
-                  //chrome.tabs.sendMessage(tabs[0].id, {data: "start"});
+            var activeURL = tabs[0].url;
+            localStorage.setItem(activeURL,'xxxx');
+            chrome.browserAction.setBadgeText({"text":localStorage.length.toString()});
+
+            chrome.tabs.update(
+              tabs[0].id, { url: 'http://localhost/?p=1'}, function(tab) {
+              //tabs[0].id, { url: 'https://www.facebook.com/en7erafatamnaldawla'}, function(tab) {
+                  //chrome.tabs.onUpdated.addListener(function listener(tabId, changeInfo) {
+                  //if (tabId === tab.id && changeInfo.status == 'complete') {
+                    //chrome.tabs.onUpdated.removeListener(listener);
+                    // Now the tab is ready!
+                    //chrome.tabs.sendMessage(tabs[0].id, {data: "start"});
+                  });
                 });
-              });
-            });
-          });
+      }
+    });
+    })
+
+    button.addEventListener('click', function () {});
