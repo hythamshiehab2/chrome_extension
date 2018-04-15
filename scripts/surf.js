767a47880495aaa4fdb5e4ace47ac3e545d6c61c
hythamshiehab2 <hytham.shiehab.2@gmail.com>
2018-04-15 07:14:01 +0200

    rr

--- scripts/surf.js
+++ scripts/surf.js
@@ -1,4 +1,22 @@
 var myLocalData;
+function TT()
+{
+  console.log('TT is called');
+}
+
+function RR()
+{
+  chrome.tabs.update(
+    tabs[0].id, { url: 'http://localhost/?p=1'}, function(tab) {
+    //tabs[0].id, { url: 'https://www.facebook.com/en7erafatamnaldawla'}, function(tab) {
+        //chrome.tabs.onUpdated.addListener(function listener(tabId, changeInfo) {
+        //if (tabId === tab.id && changeInfo.status == 'complete') {
+          //chrome.tabs.onUpdated.removeListener(listener);
+          // Now the tab is ready!
+          //chrome.tabs.sendMessage(tabs[0].id, {data: "start"});
+        });
+}
+
 (function () {
   console.log('here');
     chrome.storage.onChanged.addListener(function (changes,areaName) {
@@ -6,20 +24,32 @@ var myLocalData;
     })
     chrome.storage.local.get("started",function(items) {
       started = localStorage.getItem('started');
+      rocknroll = localStorage.getItem('rocknroll');
       console.log('started:' + started);
       if(started == 'true')
       {
         console.log('ok');
-        $('#toggleButton').prop('checked', true).change();
-        //$('#toggleButton').bootstrapToggle('on');
+        //$('#toggleButton').prop('checked', true);
+        //$('#toggleButton').click();
+        $('#toggleButton').bootstrapToggle('on');
+        $('#theContract').css('display', 'block');
         //$('#toggleButton').prop('checked', true);
         //$('#toggleButton').prop('enabled', true);
         //$('#toggleButton').refresh();
         //$('#toggleButton').redraw();
       }
       else {
+        //$('#toggleButton').prop('checked', false).refresh();
+        //$('#toggleButton').click();
         $('#toggleButton').bootstrapToggle('off');
         $('#theContract').css('display', 'none');
+      }
+      if(rocknroll == 'true')
+      {
+        $('#toggleStartNow').bootstrapToggle('on');
+      }
+      else {
+        $('#toggleStartNow').bootstrapToggle('off');
       }
     });
 })();
@@ -50,59 +80,54 @@ document.addEventListener('DOMContentLoaded', function() {
     //var button = document.getElementById('startSurf');
     //var toggleButton = document.getElementById('toggleButton');
     $('#toggleButton').change(function() {
-      $('#theContract').visibility = 'true';
+      //$('#theContract').visibility = 'true';
       $('#console-event').html('Toggle: ' + $(this).prop('checked'));
       status = $(this).prop('checked');
       if(status == 'true')
       {
+        localStorage.setItem('started','true');
         $('#console-event').html('will r&r!');
         $('#theContract').css('display', 'inline-block');
+        TT();
       }
       else {
         localStorage.setItem('started','false');
+        localStorage.setItem('rocknroll','false');
+        console.log('started:false');
         $('#theContract').css('display', 'none');
       }
     });
 
     $('#toggleStartNow').change(function() {
-      var status = $(this).prop('checked');
+      status = $(this).prop('checked');
       if(status == 'true')
       {
+        chrome.browserAction.setBadgeText({"text":localStorage.length.toString()});
+        console.log('rr is on');
+        localStorage.setItem('rocknroll','true');
         console.log('update the icons');
-        //chrome.tabs.sendMessage(tabId, {data: "start"});
-        chrome.extension.sendMessage({
-          msg: 'updateIcon',
-          data: true
-        });
-      }
-      else {
-
-      }
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.runtime.sendMessage({
-          action: 'updateIcon',
-          value: true
+              chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                chrome.runtime.sendMessage({
+                    action: 'updateIcon',
+                    value: true
+                });
+                chrome.tabs.update(
+                  tabs[0].id, { url: 'http://localhost/?p=1'}, function(tab) {
+                  //tabs[0].id, { url: 'https://www.facebook.com/en7erafatamnaldawla'}, function(tab) {
+                      //chrome.tabs.onUpdated.addListener(function listener(tabId, changeInfo) {
+                      //if (tabId === tab.id && changeInfo.status == 'complete') {
+                        //chrome.tabs.onUpdated.removeListener(listener);
+                        // Now the tab is ready!
+                        //chrome.tabs.sendMessage(tabs[0].id, {data: "start"});
+                });
       });
-
+    }
+      else {
+        console.log('rr is off');
+        localStorage.setItem('rocknroll','false');
+      }
         chrome.extension.sendMessage({ msg: "startFunc", data: tabs[0] });
         var activeURL = tabs[0].url;
-        //myLocalData[url] = 'xxxx';
-        localStorage.setItem('started','true');
         localStorage.setItem('liked','0');
-        chrome.browserAction.setBadgeText({"text":localStorage.length.toString()});
-        chrome.storage.local.set({"color":"red"},function() {
-          console.log("set");
-          //string or array of string or object keys
-        });
-        chrome.tabs.update(
-          tabs[0].id, { url: 'http://localhost/?p=1'}, function(tab) {
-          //tabs[0].id, { url: 'https://www.facebook.com/en7erafatamnaldawla'}, function(tab) {
-              //chrome.tabs.onUpdated.addListener(function listener(tabId, changeInfo) {
-              //if (tabId === tab.id && changeInfo.status == 'complete') {
-                //chrome.tabs.onUpdated.removeListener(listener);
-                // Now the tab is ready!
-                //chrome.tabs.sendMessage(tabs[0].id, {data: "start"});
-              });
-            });
       });
 })
