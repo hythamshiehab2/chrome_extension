d13143a28d02a7a9fe24fde74dc310079ed0b3cd
hythamshiehab2 <hytham.shiehab.2@gmail.com>
2018-04-22 23:00:49 +0200

    I will store tabId in the localStorage for passing inbetween
    Now there is some error when to set the tabId, specially when its not available till some late state of the runtime

--- scripts/background.js
+++ scripts/background.js
@@ -82,7 +82,7 @@ function Twitter() {
 }
 
 chrome.storage.onChanged.addListener(function (changes, namespace) {
-    updateIcon();
+    //updateIcon();
     for (key in changes) {
         var storageChange = changes[key];
         if (key == 'rocknroll') {
@@ -138,16 +138,16 @@ function updateIcon(data) {
             });
         }
     }
-//    tabId = localStorage.getItem('tabId') || false;
-//    if (!tabId) {
-//        localStorage.setItem('tabId', data.tabId);
-//        chrome.storage.local.set({
-//            'tabId': data.tabId
-//        }, function () {
-//            // Notify that we saved.
-//            console.log('surf:Settings saved');
-//        });
-//    }
+    tabId = localStorage.getItem('tabId') || false;
+    if (!tabId) {
+        localStorage.setItem('tabId', data.tabId);
+        chrome.storage.local.set({
+            'tabId': data.tabId
+        }, function () {
+            // Notify that we saved.
+            console.log('surf:Settings saved');
+        });
+    }
 }
 
 // function checkForValidURL(tabId, info, tab) {
