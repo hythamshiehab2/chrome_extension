a78c55db552da126c08589f3e6000d2a44850005
hythamshiehab2 <hytham.shiehab.2@gmail.com>
2018-05-04 00:46:06 +0200

    >>>>

--- scripts/twitter.js
+++ scripts/twitter.js
@@ -1,37 +1,54 @@
 isMomHappy = false;
-var willIGetNewPhone = new Promise(
-    function (resolve, reject) {
-        if (isMomHappy) {
-            var phone = {
-                brand: 'Samsung',
-                color: 'black'
-            };
-            resolve(phone); // fulfilled
-        } else {
-            var reason = new Error('mom is not happy');
-            reject(reason); // reject
-        }
-    }
-);
+var wait = ms => new Promise((r, j) => setTimeout(r, ms));
 
-// call our promise
-function x() {
-    var askMom = function () {
-        willIGetNewPhone
-            .then(function (fulfilled) {
-                // yay, you got a new phone
-                console.log(fulfilled);
-                // output: { brand: 'Samsung', color: 'black' }
-            })
-            .catch(function (error) {
-                // oops, mom don't buy it
-                console.log(error.message);
-                // output: 'mom is not happy'
-            });
+function test() {
+    var b = null;
+    //var willIGetNewPhone = new Promise(
+    var willGetNewPhone = function () {
+        return new Promise((resolve, reject) => {
+            console.log('here');
+            //setTimeout(() => {
+            b = document.getElementById('global-new-tweet-button');
+            if (b) {
+                console.log('should not come too early');
+                resolve(b);
+            } else {
+                console.log('not found');
+                var reason = new Error('mom is not happy');
+                reject(reason); // reject
+            }
+            //}, 10000);
+            console.log('after timeout');
+            console.log(b);
+        });
     };
+
+    const tweetButtonClicked = function (b) {
+        var c = document.getElementById('comment');
+        console.log(c);
+        return Promise.resolve(c);
+    };
+    willGetNewPhone
+        //.then(function (fulfilled) {
+        .then(tweetButtonClicked);
+    // yay, you got a new phone
+    //console.log(fulfilled);
+    // output: { brand: 'Samsung', color: 'black' }
+    //})
+    //.catch(function (error) {
+    // oops, mom don't buy it
+    //console.log(error.message);
+    // output: 'mom is not happy'
+    //});
 }
+
 $(document).ready(function () {
-    //setTimeout(askMom, 30000);
+    //setTimeout(begin, 30000);
+    begin();
 });
 
-function begin() {}
+function begin() {
+    console.log('will wait 10s');
+    wait(30000);
+    test();
+}
