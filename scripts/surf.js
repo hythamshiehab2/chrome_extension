dbeb088025226ba0961240bb082fb8b37e148834
hythamshiehab2 <hytham.shiehab.2@gmail.com>
2018-05-04 21:13:05 +0200

    when big on is off, all is off

--- scripts/surf.js
+++ scripts/surf.js
@@ -18,105 +18,107 @@ function alertFinished() {
 window.addEventListener("load", register);
 //window.onload = function() {
 function moduleLoadProgress(event) {
-  var loadPercent = 0.0;
-  var loadPercentString;
-  if (event.lengthComputable && event.total > 0) {
-    loadPercent = event.loaded / event.total * 100.0;
-    loadPercentString = loadPercent + '%';
-    common.logMessage('progress: ' + event.url + ' ' + loadPercentString +
-                     ' (' + event.loaded + ' of ' + event.total + ' bytes)');
-  } else {
-    // The total length is not yet known.
-    common.logMessage('progress: Computing...');
-  }
+    var loadPercent = 0.0;
+    var loadPercentString;
+    if (event.lengthComputable && event.total > 0) {
+        loadPercent = event.loaded / event.total * 100.0;
+        loadPercentString = loadPercent + '%';
+        common.logMessage('progress: ' + event.url + ' ' + loadPercentString +
+            ' (' + event.loaded + ' of ' + event.total + ' bytes)');
+    } else {
+        // The total length is not yet known.
+        common.logMessage('progress: Computing...');
+    }
 }
 
 function register() {
-//$(document).ready(function() {
-  console.log('document is loaded');
-  console.log('surf:here');
-  started = localStorage.getItem('started') || 'false';
-  rocknroll = localStorage.getItem('rocknroll') || 'false';
-  console.log('surf:started:' + started);
-  if (started === 'true') {
-      $('#toggleButton').bootstrapToggle('on');
-      $('#theContract').css('display', 'block');
-  } else {
-      $('#toggleButton').bootstrapToggle('off');
-      $('#theContract').css('display', 'none');
-  }
-  if (rocknroll === 'true') {
-      $('#toggleStartNow').bootstrapToggle('on');
-  } else {
-      $('#toggleStartNow').bootstrapToggle('off');
-  }
+    //$(document).ready(function() {
+    console.log('document is loaded');
+    console.log('surf:here');
+    started = localStorage.getItem('started') || 'false';
+    rocknroll = localStorage.getItem('rocknroll') || 'false';
+    console.log('surf:started:' + started);
+    if (started === 'true') {
+        $('#toggleButton').bootstrapToggle('on');
+        $('#theContract').css('display', 'block');
+    } else {
+        $('#toggleButton').bootstrapToggle('off');
+        $('#toggleStartNow').bootstrapToggle('off');
+        $('#theContract').css('display', 'none');
+    }
+    if (rocknroll === 'true') {
+        $('#toggleStartNow').bootstrapToggle('on');
+    } else {
+        $('#toggleStartNow').bootstrapToggle('off');
+    }
 
-  $('#toggleButton').change(function () {
-      $('#console-event').html('Toggle: ' + $(this).prop('checked'));
-      status = $(this).prop('checked');
-      if (status == 'true') {
-          localStorage.setItem('started', 'true');
-          chrome.storage.local.set({
-              'started': 'true'
-          }, function () {
-              // Notify that we saved.
-              console.log('surf:Settings saved');
-          });
-          $('#console-event').html('will r&r!');
-          $('#theContract').css('display', 'block');
-      } else {
-          localStorage.setItem('started', 'false');
-          localStorage.setItem('rocknroll', 'false');
-          chrome.storage.local.set({
-              'started': 'false',
-              'rocknroll': 'false'
-          }, function () {
-              // Notify that we saved.
-              console.log('surf:Settings saved');
-          });
-          console.log('started:false');
-          $('#theContract').css('display', 'none');
-      }
-  });
+    $('#toggleButton').change(function () {
+        $('#console-event').html('Toggle: ' + $(this).prop('checked'));
+        status = $(this).prop('checked');
+        if (status == 'true') {
+            localStorage.setItem('started', 'true');
+            chrome.storage.local.set({
+                'started': 'true'
+            }, function () {
+                // Notify that we saved.
+                console.log('surf:Settings saved');
+            });
+            $('#console-event').html('will r&r!');
+            $('#theContract').css('display', 'block');
+        } else {
+            localStorage.setItem('started', 'false');
+            localStorage.setItem('rocknroll', 'false');
+            chrome.storage.local.set({
+                'started': 'false',
+                'rocknroll': 'false'
+            }, function () {
+                // Notify that we saved.
+                console.log('surf:Settings saved');
+            });
+            console.log('started:false');
+            $('#toggleStartNow').bootstrapToggle('off');
+            $('#theContract').css('display', 'none');
+        }
+    });
 
-  $('#toggleStartNow').change(function () {
-      status = $(this).prop('checked');
-      if (status == 'true') {
-          chrome.browserAction.setBadgeText({
-              "text": localStorage.length.toString()
-          });
-          localStorage.setItem('rocknroll', 'true');
-          chrome.storage.local.set({
-              'rocknroll': 'true'
-          }, function () {
-              // Notify that we saved.
-              console.log('surf:Settings saved');
-          });
-          chrome.runtime.sendMessage({
-              data: "surfStartIgnition"
-          }, function (response) {
-              console.log("from background:" + response);
-              console.log(response);
-          });
-      } else {
-          console.log('rr is off');
-          localStorage.setItem('rocknroll', 'false');
-          chrome.storage.local.set({
-              'rocknroll': 'false'
-          }, function () {
-              // Notify that we saved.
-              console.log('surf:Settings saved');
-          });
-      }
-      localStorage.setItem('liked', '0');
-      chrome.storage.local.set({
-          'liked': '0'
-      }, function () {
-          // Notify that we saved.
-          console.log('surf:Settings saved');
-      });
-  });
-//});
+    $('#toggleStartNow').change(function () {
+        status = $(this).prop('checked');
+        if (status == 'true') {
+            chrome.browserAction.setBadgeText({
+                "text": localStorage.length.toString()
+            });
+            localStorage.setItem('rocknroll', 'true');
+            chrome.storage.local.set({
+                'rocknroll': 'true'
+            }, function () {
+                // Notify that we saved.
+                console.log('surf:Settings saved');
+            });
+            chrome.runtime.sendMessage({
+                data: "surfStartIgnition"
+            }, function (response) {
+                console.log("from background:" + response);
+                console.log(response);
+            });
+        } else {
+            console.log('rr is off');
+            localStorage.setItem('rocknroll', 'false');
+            chrome.storage.local.set({
+                'rocknroll': 'false'
+            }, function () {
+                // Notify that we saved.
+                console.log('surf:Settings saved');
+            });
+        }
+        localStorage.setItem('liked', '0');
+        chrome.storage.local.set({
+            'liked': '0'
+        }, function () {
+            // Notify that we saved.
+            console.log('surf:Settings saved');
+        });
+    });
+    //});
 
 }
 
