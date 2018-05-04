36c71311b059bd8c89ae719d5977b27670674f07
hythamshiehab2 <hytham.shiehab.2@gmail.com>
2018-05-04 14:58:05 +0200

    I think twitter.js is injected pre early

--- scripts/twitter.js
+++ scripts/twitter.js
@@ -1,69 +1,100 @@
 isMomHappy = false;
 var wait = ms => new Promise((r, j) => setTimeout(r, ms));
 
-function getData() {
-    return new Promise((resolve, reject) => {
-        var b = document.getElementById('global-new-tweet-button');
-        if (b) {
-            console.log('i got it');
-            resolve(b);
-        } else {
-            console.log('missed');
-            reject(b);
-        }
-    });
-}
+var originalPromise = new Promise(function (resolve,reject) {
+	var b = null;
+	//setTimeout(function() {
+	        b = document.getElementById('global-new-tweet-button');
+		if(b)
+		{
+			console.log(b);
+			resolve(b);
+		}
+		else
+		{
+			console.log('negative');
+			reject('negative');
+		}
+	//}, 10000);
+});
 
-function test() {
-    var b = null;
-    //var willIGetNewPhone = new Promise(
-    var willGetNewPhone = function () {
-        return new Promise((resolve, reject) => {
-            console.log('here');
-            //setTimeout(() => {
-            b = document.getElementById('global-new-tweet-button');
-            if (b) {
-                console.log('should not come too early');
-                return Promise.resolve(b);
-            } else {
-                console.log('not found');
-                var reason = new Error('mom is not happy');
-                reject(reason); // reject
-            }
-            //}, 10000);
-            console.log('after timeout');
-            console.log(b);
-        });
-    };
-
-    const tweetButtonClicked = function (b) {
-        var c = document.getElementById('comment');
-        console.log(c);
-        return Promise.resolve(c);
-    };
-    willGetNewPhone
-        //.then(function (fulfilled) {
-        .then(tweetButtonClicked);
-    // yay, you got a new phone
-    //console.log(fulfilled);
-    // output: { brand: 'Samsung', color: 'black' }
-    //})
-    //.catch(function (error) {
-    // oops, mom don't buy it
-    //console.log(error.message);
-    // output: 'mom is not happy'
-    //});
+function callItAgain() {
+	var myPromise = MakeQuerablePromise(originalPromise);
+	myPromise
+	.then(function(data){
+	    console.log(data); // "Yeah !"
+	    console.log("Final fulfilled:", myPromise.isFulfilled());//true
+	    console.log("Final rejected:", myPromise.isRejected());//false
+	    console.log("Final pending:", myPromise.isPending());//false
+	})
+	.catch(function(data){
+	   console.log(data);		
+		if(data === 'negative')
+		{
+			console.log('will call it again');
+			setTimeout(callItAgain,5000);
+		}
+	});
 }
 
 $(document).ready(function () {
     //setTimeout(begin, 30000);
     //begin();
     //getData();
+	console.log('document ready');
+	var myPromise = MakeQuerablePromise(originalPromise);
+	console.log("Initial fulfilled:", myPromise.isFulfilled());//false
+	console.log("Initial rejected:", myPromise.isRejected());//false
+	console.log("Initial pending:", myPromise.isPending());//true
+
+	myPromise
+	.then(function(data){
+	    console.log(data); // "Yeah !"
+	    console.log("Final fulfilled:", myPromise.isFulfilled());//true
+	    console.log("Final rejected:", myPromise.isRejected());//false
+	    console.log("Final pending:", myPromise.isPending());//false
+	})
+	.catch(function(data){
+		if(data === 'negative')
+		{
+			callItAgain();
+		}
+	   console.log(data);		
+	});
 });
 
 function begin() {
     console.log('at begin');
     //wait(30000);
     //test();
-    getData();
+    //var x = getData();
+}
+
+function MakeQuerablePromise(promise) {
+    // Don't modify any promise that has been already modified.
+    if (promise.isResolved) return promise;
+
+    // Set initial state
+    var isPending = true;
+    var isRejected = false;
+    var isFulfilled = false;
+
+    // Observe the promise, saving the fulfillment in a closure scope.
+    var result = promise.then(
+        function(v) {
+            isFulfilled = true;
+            isPending = false;
+            return v; 
+        }, 
+        function(e) {
+            isRejected = true;
+            isPending = false;
+            throw e; 
+        }
+    );
+
+    result.isFulfilled = function() { return isFulfilled; };
+    result.isPending = function() { return isPending; };
+    result.isRejected = function() { return isRejected; };
+    return result;
 }
