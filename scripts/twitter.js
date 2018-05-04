6f3d88381d920a03cda59905e29b6b85a0351aa8
hythamshiehab2 <hytham.shiehab.2@gmail.com>
2018-05-04 02:18:43 +0200

    waiting for internet connection to test...

--- scripts/twitter.js
+++ scripts/twitter.js
@@ -3,9 +3,9 @@ var wait = ms => new Promise((r, j) => setTimeout(r, ms));
 
 function getData() {
     return new Promise((resolve, reject) => {
-        var b = document.getElementById('x');
+        var b = document.getElementById('global-new-tweet-button');
         if (b) {
-            consoel.log('i got it');
+            console.log('i got it');
             resolve(b);
         } else {
             console.log('missed');
@@ -24,7 +24,7 @@ function test() {
             b = document.getElementById('global-new-tweet-button');
             if (b) {
                 console.log('should not come too early');
-                resolve(b);
+                return Promise.resolve(b);
             } else {
                 console.log('not found');
                 var reason = new Error('mom is not happy');
@@ -65,4 +65,5 @@ function begin() {
     console.log('at begin');
     //wait(30000);
     //test();
+    getData();
 }
