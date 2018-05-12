dba97c6921ba9653da143f76e8c9cd4b49808503
هيثم شهاب <hytham.shiehab.2@gmail.com>
2018-05-12 03:22:41 +0200

    moving listener to online status at content.js

--- scripts/content.js
+++ scripts/content.js
@@ -1,31 +1,30 @@
 if (navigator.onLine) {
     console.log('ONLINE!');
+    chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+        var data = request.data || {};
+        console.log(
+            sender.tab ?
+            "from a content script:" + sender.tab.url :
+            "from the extension");
+        console.log(request);
+        console.log('content.js:' + data);
+
+        if (data === 'doMaharaStuff_REQ') {
+            var responseObject = {
+                message: "doMaharaStuff_REQ:CONFIRMED",
+            };
+            sendResponse(responseObject);
+        }
+
+        if (data === 'doTwitterStuff_REQ') {
+            console.log('doTwitterStuff_REQ RECIEVED');
+            console.log('SENDING CONFIRMATION')
+            var responseObject = {
+                message: "doTwitterStuff_REQ:CONFIRMED",
+            };
+            sendResponse(responseObject);
+        }
+    });
 } else {
     console.log('Connection flaky');
 }
-
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    var data = request.data || {};
-    console.log(
-        sender.tab ?
-        "from a content script:" + sender.tab.url :
-        "from the extension");
-    console.log(request);
-    console.log('content.js:' + data);
-
-    if (data === 'doMaharaStuff_REQ') {
-        var responseObject = {
-            message: "doMaharaStuff_REQ:CONFIRMED",
-        };
-        sendResponse(responseObject);
-    }
-
-    if (data === 'doTwitterStuff_REQ') {
-        console.log('doTwitterStuff_REQ RECIEVED');
-        console.log('SENDING CONFIRMATION')
-        var responseObject = {
-            message: "doTwitterStuff_REQ:CONFIRMED",
-        };
-        sendResponse(responseObject);
-    }
-});
