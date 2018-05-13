53b6c11f255532d9ce0e52e738e0db9c5a3f7e03
هيثم شهاب <hytham.shiehab.2@gmail.com>
2018-05-14 00:32:12 +0200

    manifest update

--- scripts/background.js
+++ scripts/background.js
@@ -100,7 +100,7 @@ chrome.runtime.onInstalled.addListener(function () {
 
 
 function nextRunSchedule() {
-    var r = Math.floor(Math.random() * 60) + 30;
+    var r = Math.floor(Math.random() * 60) + 60;
     r *= 1000 * 60;
     //var r = 600000;
     nextRunTimer = setTimeout(rollTheDice, r);
